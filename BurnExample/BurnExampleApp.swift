ec6b2f3f9b421efb49d9befe361d16e4a3d6bd18
AH <AnthonyBryanHarvey@gmail.com>
2023-04-21 13:54:07 -0400

    Initial Commit

---
+++ BurnExample/BurnExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurnExampleApp.swift
+//  BurnExample
+//
+//  Created by Anthony Harvey on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct BurnExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
